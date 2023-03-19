022fe8c0476bffa93fb03238a7fa00443d0956d9
Pato Miner <patominer@gmail.com>
2023-03-19 22:24:14 +1300

    Primer commit!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Modal
+} from 'react-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
+
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [dynamicButtonId, setDynamicButtonId] = React.useState(uuidv4());
+  const [hiddenElementVisible, setHiddenElementVisible] = React.useState(false);
+
+  const handleDynamicButtonClick = () => {
+    setDynamicButtonId(uuidv4());
+
+    setHiddenElementVisible(false);
+    setTimeout(() => {
+      setHiddenElementVisible(true);
+    }, 3000); // El elemento oculto se mostrará después de 3 segundos
+  };
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="mt-5">
+        <Col>
+          <h1>Free Range Testers Sandbox</h1>
+          <p>Visita nuestra página en <a href="https://www.freerangetesters.com" target="_blank" rel="noopener noreferrer">www.freerangetesters.com</a> para obtener más información.</p>
+          <p>Utiliza esta página para practicar automation testing con herramientas como Selenium, Cypress, Playwright y Katalon Studio.</p>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <h2>Botón con ID dinámico y elemento oculto</h2>
+          <Button id={dynamicButtonId} onClick={handleDynamicButtonClick}>
+            Clic para generar ID dinámico y mostrar elemento oculto
+          </Button>
+          {hiddenElementVisible && (
+            <p id="hidden-element" className="mt-3">
+              Este es un elemento oculto que se muestra después de 3 segundos.
+            </p>
+          )}
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <Form>
+            <Form.Group controlId="formBasicText">
+              <Form.Label>Texto</Form.Label>
+              <Form.Control type="text" placeholder="Ingresa texto" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Checkbox" />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Radio buttons</Form.Label>
+              <Form.Check
+                type="radio"
+                label="Opción 1"
+                name="formRadioOptions"
+                id="formRadio1"
+              />
+              <Form.Check
+                type="radio"
+                label="Opción 2"
+                name="formRadioOptions"
+                id="formRadio2"
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicSelect">
+              <Form.Label>Dropdown</Form.Label>
+              <Form.Control as="select">
+                <option>Selecciona una opción</option>
+                <option>Opción 1</option>
+                <option>Opción 2</option>
+                <option>Opción 3</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <DropdownButton id="dropdown-basic-button" title="Dropdown">
+            <Dropdown.Item href="#/action-1">Acción 1</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Acción 2</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Acción 3</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <Button variant="primary" onClick={() => setModalShow(true)}>
+            Mostrar popup
+          </Button>
+
+          <Modal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title-vcenter">
+                Popup de ejemplo
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <p>
+                Este es un popup de ejemplo para practicar automation testing.
+              </p>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => setModalShow(false)}>Cerrar</Button>
+            </Modal.Footer>
+          </Modal>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col>
+          <div id="shadow-root-example">
+            <h2>Shadow DOM</h2>
+            <div id="shadow-host"></div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Crear un shadow DOM
+const shadowHost = document.getElementById('shadow-host');
+const shadowRoot = shadowHost.attachShadow({ mode: 'open' });
+
+const shadowContent = document.createElement('div');
+shadowContent.innerHTML = '<p>Este es un ejemplo de Shadow DOM para practicar automation testing.</p>';
+shadowRoot.appendChild(shadowContent);
+
 reportWebVitals();
